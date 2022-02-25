9501a092d1a4f7105d1a216a1f181e80b5bf7723
vallusomasekhar43 <vallusomasekhar43@gmail.com>
2022-02-25 10:53:44 +0530

    Changed Fibonacci series log

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -3,7 +3,7 @@ package simple.example.problems;
 public class FibonacciSeries {
     public static void main(String[] args) {
         int n =10,firstTerm=0,secondTerm=1;
-        System.out.println("Fibonacci series till"+n+"terms");
+        System.out.println("Series is "+n+"terms");
 
         for(int i=0;i<=n;++i){
             System.out.printf(firstTerm+",");
